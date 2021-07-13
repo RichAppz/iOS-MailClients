feaa97da6b01307cb8c7fd096c22b49fd769d1e3
Rich Mucha <rich@richappz.com>
2021-07-13 15:28:01 +0100

    Spelling issue

--- iOS-MailClients/Extension+String.swift
+++ iOS-MailClients/Extension+String.swift
@@ -31,7 +31,7 @@ extension String {
     static var cancel: String { return "Cancel" }
     static var ok: String { return "Ok" }
     static var emailQuestion: String { return "How do you check your mail?" }
-    static var noEmailAccountsMessage: String { return "No email apps were found on your device." }
+    static var noEmailAccountsMessage: String { return "No email apps were found on your device" }
     static var copiedEmailAddress: String { return ", %@ has been copied to your clipboard." }
     static var and: String { return "&" }
     static var chooseEmailApp: String { return "Choose the email app to open" }

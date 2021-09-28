f851ab424bdac42c29fa07416ba7720a7dd9c60d
Rich Mucha <rich@richappz.com>
2021-09-28 09:27:48 +0100

    Update

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "iOS-MailClients",
+    name: "iOSMailClients",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "iOS-MailClients",
+            name: "iOSMailClients",
             targets: ["iOS-MailClients"]),
     ],
     dependencies: [
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "iOS-MailClients",
+            name: "iOSMailClients",
             dependencies: []),
         .testTarget(
             name: "iOS-MailClientsTests",
